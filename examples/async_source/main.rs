05d7398f838a192409521ffd84b798f857d2f9bf
developerworks <developerworks@163.com>
2022-10-21 21:10:25 +0800

    Add an option of get through proxy

--- examples/async_source/main.rs
+++ examples/async_source/main.rs
@@ -35,17 +35,18 @@ async fn run_server() -> Result<(), Box<dyn Error>> {
 //////////////////////
 async fn run_client() -> Result<(), Box<dyn Error>> {
     // Good enough for an example to allow server to start
-    tokio::time::sleep(tokio::time::Duration::from_secs(3)).await;
+    tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
 
     let config = ConfigBuilder::<AsyncState>::default()
         .add_async_source(HttpAsyncSource {
-            uri: "http://localhost:5001/configuration".into(),
+            uri: "https://raw.githubusercontent.com/developerworks/crate_demo_config/main/resources/config.json".into(),
             format: FileFormat::Json,
         })
         .build()
         .await?;
 
-    println!("Config value is {}", config.get::<String>("value")?);
+    // println!("Config value is {}", config.get::<String>("value")?);
+    println!("Config value is {:#?}", config);
 
     Ok(())
 }
@@ -61,7 +62,11 @@ struct HttpAsyncSource<F: Format> {
 #[async_trait]
 impl<F: Format + Send + Sync + Debug> AsyncSource for HttpAsyncSource<F> {
     async fn collect(&self) -> Result<Map<String, config::Value>, ConfigError> {
-        reqwest::get(&self.uri)
+        let client = get_client(true);
+
+        client
+            .get(&self.uri)
+            .send()
             .await
             .map_err(|e| ConfigError::Foreign(Box::new(e)))? // error conversion is possible from custom AsyncSource impls
             .text()
@@ -72,5 +77,21 @@ impl<F: Format + Send + Sync + Debug> AsyncSource for HttpAsyncSource<F> {
                     .parse(Some(&self.uri), &text)
                     .map_err(|e| ConfigError::Foreign(e))
             })
+    }
+}
+
+fn get_client(use_proxy: bool) -> reqwest::Client {
+    let client_builder = reqwest::Client::builder();
+    if use_proxy {
+        let proxy =
+            reqwest::Proxy::https("http://192.168.1.110:1087").expect("tor proxy should be there");
+        client_builder
+            .proxy(proxy)
+            .build()
+            .expect("should be able to build reqwest client")
+    } else {
+        client_builder
+            .build()
+            .expect("should be able to build reqwest client")
     }
 }
